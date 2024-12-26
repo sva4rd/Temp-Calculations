3985d8578d70f063cd0dc4d8f1f09d124e48df0a
sva4rd <egor71175@gmail.com>
2024-12-26 22:55:02 +0300

    upd cors

--- src/main/java/by/spaces/calculator/Main.java
+++ src/main/java/by/spaces/calculator/Main.java
@@ -18,7 +18,7 @@ public class Main {
             @Override
             public void addCorsMappings(    CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("*");
             }
         };
